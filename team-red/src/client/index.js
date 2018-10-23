850441e436d7ffded2662e53220f0bd7984020a6
Daniel Dughila <daniel.dughila@3PillarGlobal.com>
2018-10-23 15:44:25 +0300

    Remove cruft

--- team-red/src/client/index.js
+++ team-red/src/client/index.js
@@ -13,32 +13,3 @@ const app = document.getElementById('team-red-app');
  */
 
 hydrate(<Spaceships />, app);
-
-
-window.postMessage(
-  {
-    name: 'BUY_SPACESHIP',
-    payload: {
-      id: 'sonJet',
-      name: 'Son Jet',
-      price: 1000,
-      url: 'assets/sonJet.jpg',
-      quantity: 1
-    },
-    via: 'TEAM_RED_TUNNEL'
-  },
-  process.env.SKELETON_URL
-);
-
-
-window.addEventListener('message', (event) => {
-  if (
-    event
-    && event.origin === process.env.SKELETON_URL
-    && event.data
-    && event.data.via === 'TEAM_BLUE_TUNNEL'
-    && event.data.name === 'BUDGET_EXCEEDED'
-  ) {
-    alert('BUDGET_EXCEEDED');
-  }
-});
